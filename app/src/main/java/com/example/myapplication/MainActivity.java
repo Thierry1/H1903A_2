0691a7f0bf82c2daf71d3a02aa987b3b0a085a54
Thierry1 <floveandroid@sina.com>
2019-10-23 17:13:21 +0800

    今天实现了很多功能该下班了

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -21,4 +21,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
 }
